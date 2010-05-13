1f12b61c43e9c5adb791d1ac526b99698f04f1f7
Togo Harrian <calmacil@Fondcombe.(none)>
2010-05-13 19:48:18 +0200

    first commit

---
+++ src/nojserv/NojServ.java
@@ -0,0 +1,13 @@
+package nojserv;
+
+public class NojServ {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Coucou");
+	}
+
+}
